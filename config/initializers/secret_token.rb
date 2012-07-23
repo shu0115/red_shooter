775be8232f1c5b11ccf09fe9cff06c4994321c87
shu0115 <raisondetre0115@gmail.com>
2012-07-23 16:20:34 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedShooter::Application.config.secret_token = '6c62330fd96901021570f6b1c642d1cf5122258577d45e3b10289af5c81ddea5aa36b6f322e8fd51584abd338f9e7ea0ea37a1f3619f529b5009a5c6c886df29'
